17cb31297a63b652b0f0244e5ebb3c4c328d2c3b
Gourav Saxena <gourav.saxena@kaltura.com>
2019-05-22 14:20:03 +0530

    - AndroidX migration

--- kavaplugin/src/main/java/com/kaltura/playkit/plugins/kava/DataHandler.java
+++ kavaplugin/src/main/java/com/kaltura/playkit/plugins/kava/DataHandler.java
@@ -1,7 +1,8 @@
 package com.kaltura.playkit.plugins.kava;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+
+import androidx.annotation.Nullable;
 
 import com.kaltura.playkit.PKError;
 import com.kaltura.playkit.PKEvent;
