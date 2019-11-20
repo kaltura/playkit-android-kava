7580bb9e9e4f96e989d8e29863d56190939cf4cb
Gilad Nadav <gilad.nadav@kaltura.com>
2019-11-20 15:31:58 +0200

    FEM-2728 - protect case where switch fails to get the media format as an object so give a default then resolving it

--- kavaplugin/src/main/java/com/kaltura/playkit/plugins/kava/DataHandler.java
+++ kavaplugin/src/main/java/com/kaltura/playkit/plugins/kava/DataHandler.java
@@ -303,14 +303,18 @@ class DataHandler {
      * @param event - current event.
      */
     void handleSourceSelected(PKEvent event) {
+        deliveryType = StreamFormat.Url.formatName;
         PKMediaSource selectedSource = ((PlayerEvent.SourceSelected) event).source;
-        switch (selectedSource.getMediaFormat()) {
-            case dash:
-            case hls:
-                deliveryType = selectedSource.getMediaFormat().name();
-                break;
-            default:
-                deliveryType = StreamFormat.Url.formatName;
+        if (selectedSource != null && selectedSource.getMediaFormat() != null) {
+            PKMediaFormat selectedSourceMediaFormat = selectedSource.getMediaFormat();
+            switch (selectedSourceMediaFormat) {
+                case dash:
+                case hls:
+                    deliveryType = selectedSourceMediaFormat.name();
+                    break;
+                default:
+                    deliveryType = StreamFormat.Url.formatName;
+            }
         }
     }
 
